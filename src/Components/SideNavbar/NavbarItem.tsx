341cc93d193255e6d647b3c2aa8e5d40b80e23aa
Osama Almaani <osama.maani@gmail.com>
2020-08-22 22:21:19 +0300

    background gradiant on active nav item (Dummy)

--- src/Components/SideNavbar/NavbarItem.tsx
+++ src/Components/SideNavbar/NavbarItem.tsx
@@ -5,14 +5,19 @@ const NavbarItem = ({
 	subtitle,
 	color,
 	icon,
+	onClick,
+	active = false,
 }: {
 	title: String;
 	subtitle: String;
 	color: 'blue' | 'purple' | 'green' | 'black';
 	icon: ReactNode;
+	onClick: any;
+	active?: Boolean;
 }) => {
 	return (
-		<div className='navbar-item-container'>
+		// @ts-nocheck
+		<div className={`navbar-item-container ${active ? 'active' : ''}`} onClick={onClick}>
 			<div className={`icon-container ${color}`}>{icon}</div>
 			<div className='title-container'>
 				<h5 className='title'>{title}</h5>
--- src/Components/SideNavbar/SideNavbar.tsx
+++ src/Components/SideNavbar/SideNavbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BiSupport, BiSearchAlt2 } from 'react-icons/bi';
 import { TiTicket } from 'react-icons/ti';
 import { AiOutlineQuestionCircle } from 'react-icons/ai';
@@ -7,7 +7,18 @@ import { HiOutlineChatAlt } from 'react-icons/hi';
 import './SideNavbar.scss';
 import NavbarItem from './NavbarItem';
 
+// TODO: Remove the dummy demo state/click handler drilling and set things in redux
+
+enum ActivePage {
+	MyTickets,
+	HelpCenter,
+	PersonalAssistant,
+	FAQs,
+}
+
 const SideNavbar = () => {
+	const [activePage, setActivePage] = useState(ActivePage.MyTickets);
+
 	return (
 		<div className='side-navbar'>
 			<h2 className='title'>Help & Support</h2>
@@ -23,24 +34,32 @@ const SideNavbar = () => {
 				title='My Tickets'
 				subtitle='See the status of your existing tickets here'
 				color='blue'
+				active={activePage === ActivePage.MyTickets}
+				onClick={() => setActivePage(ActivePage.MyTickets)}
 			/>
 			<NavbarItem
 				icon={<BiSupport size={26} color='#ffffff' />}
 				title='Help Center'
 				subtitle='Issue-tree of up to three levels'
 				color='purple'
+				active={activePage === ActivePage.HelpCenter}
+				onClick={() => setActivePage(ActivePage.HelpCenter)}
 			/>
 			<NavbarItem
 				icon={<HiOutlineChatAlt size={26} color='#ffffff' />}
 				title='Personal Assistant'
 				subtitle='You can call, request call-back or chat with us'
 				color='green'
+				active={activePage === ActivePage.PersonalAssistant}
+				onClick={() => setActivePage(ActivePage.PersonalAssistant)}
 			/>
 			<NavbarItem
 				icon={<AiOutlineQuestionCircle size={26} color='#ffffff' />}
 				title='FAQs'
 				subtitle='Find the frequently asked questions here'
 				color='black'
+				active={activePage === ActivePage.FAQs}
+				onClick={() => setActivePage(ActivePage.FAQs)}
 			/>
 		</div>
 	);
